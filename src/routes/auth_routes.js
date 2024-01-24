cccbb8e4341ee11e08ab01b233fbe37c538d0d0c
JonasB91 <jonas.backas@hotmail.com>
2024-01-24 10:13:38 +0100

    Auth_Routes och ska hämta users, och fixa login post requesten

--- src/routes/auth_routes.js
+++ src/routes/auth_routes.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
+const fs = require('fs');
+const bcrypt = require('bcrypt');
 
 const router = express.Router();
+const secretKey = 'your_secret_key'; // Ersätt denna key med en stark och säker nyckel senare!!!
+
+//Ladda user data från user.json filen där vi sparar all mockdata om users
+const userData = JSON.parse(fs.readFileSync('users.json', 'utf-8'));
+
+
+//Om vi ska ha registration så ska det ligga här!
+
+
 
 router.post('/login', (req, res) => {
+    //Condition för att kolla om user är valid med email och password, JWT Token sign, vi ska ge session id och tid för token expiration
+    //annars om inte valid user skicka felkod 401. 
+});
+
+
+
+
+/* router.post('/login', (req, res) => {
   const { email, password } = req.body;
 
   if (isValidUser(email, password)) {
@@ -20,6 +39,6 @@ const isValidUser = (email, password) => {
 
 const getUserRole = (email) => {
   return email === 'admin@example.com' ? 'admin' : 'user';
-};
+}; */
 
 module.exports = router;
