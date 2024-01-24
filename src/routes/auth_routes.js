341401e7da49987335cda893acbff9c0e1405dea
JonasB91 <jonas.backas@hotmail.com>
2024-01-24 12:14:19 +0100

    Lägger till senaste sparade ändringar jag gjorde, ändrade om user.json samt ordnade till login post requesten med jwt token

--- src/routes/auth_routes.js
+++ src/routes/auth_routes.js
@@ -2,27 +2,42 @@ const express = require('express');
 const jwt = require('jsonwebtoken');
 const fs = require('fs');
 const bcrypt = require('bcrypt');
+const env = require('dotenv');
 
 const router = express.Router();
-const secretKey = 'your_secret_key'; // Ersätt denna key med en stark och säker nyckel senare!!!
+env.config();
+const secretKey = process.env.SECRET_KEY || 'default_secret_key'; // Ersätt denna key med en stark och säker nyckel senare!!!
+
+
 
 //Ladda user data från user.json filen där vi sparar all mockdata om users
-const userData = JSON.parse(fs.readFileSync('users.json', 'utf-8'));
+const userDataPath = `${__dirname}/../user.json`;
+const userData = JSON.parse(fs.readFileSync(userDataPath, 'utf-8'));
 
 
 //Om vi ska ha registration så ska det ligga här!
 
 
 // Login endpoint där vi ska hantera login requesten och ge ett JWT Token?
+// Hashning av password? där vi ska använda oss av bcrypts ramverk för att hasha och stora dom i vår jsonfil,
 router.post('/login', (req, res) => {
-    //Condition för att kolla om user är valid med email och password, JWT Token sign, vi ska ge session id och tid för token expiration
-    //annars om inte valid user skicka felkod 401. 
+    const  { username, password } = req.body;
+
+    // gå in i userData jsonfil, och kolla users, username, condition för att kolla om password är korrekt utfärda token, annars felkod 401.
+    const user = userData.users.find((u) => u.username === username);
+
+      if( user && bcrypt.compareSync(password, user.password)) {
+        //Om Password och user är korrekt ska vi sätta utfärda ett JWT Token, med en expireDate på 1h, annars skickar vi felkod 401. att loggin blev fel!
+        const token = jwt.sign({ userId: user.userId, username: user.username }, secretKey, { expiresIn: '1h' });
+        res.json({ token });
+        console.log(token)
+      } else {
+        res.status(401).json({ error: 'Invalid Login'})
+      }
 });
 
-//Middleware function för att authentisera JWT Token
-
-
-
+//Validering och authentisering med middleware JWT Token läggs in i backend data_routes!!! 
+// Lägger en kommentar om detta bara sålänge så man kommer ihåg.
 
 
 
--- src/server.js
+++ src/server.js
@@ -4,11 +4,14 @@ const authRoutes = require('./routes/auth_routes');
 const cors = require('cors'); 
 const helmet = require ('helmet');
 
+
 const app = express();
 
 app.use(helmet());
 app.use(cors()); 
 app.use(bodyParser.json());
 app.use('/auth', authRoutes);
+
+
 
 module.exports = app;
