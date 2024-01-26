e84d085085139615601ce7120b619ae73f5d0180
JonasB91 <jonas.backas@hotmail.com>
2024-01-26 09:35:17 +0100

    Pushar upp det senaste versionen av auth som fungerar

--- src/routes/auth_routes.js
+++ src/routes/auth_routes.js
@@ -1,34 +1,37 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const fs = require('fs');
-const bcrypt = require('bcrypt');
+//const bcrypt = require('bcrypt');
 const env = require('dotenv');
+env.config();
 
 const router = express.Router();
-const secretKey = process.env.SECRET_KEY || 'default_secret_key'; // Ersätt denna key med en stark och säker nyckel senare!!!
+const SECRET_KEY = process.env.SECRET_KEY || 'yourFallbackSecretKey';
+//const SECRET_KEY = 'yourSecretKey';
 
 //Ladda user data från user.json filen där vi sparar all mockdata om users
 const userDataPath = `${__dirname}/../user.json`;
 const userData = JSON.parse(fs.readFileSync(userDataPath, 'utf-8'));
 
-
-//Om vi ska ha registration så ska det ligga här!
-
-
 // Login endpoint där vi ska hantera login requesten och ge ett JWT Token?
 // Hashning av password? där vi ska använda oss av bcrypts ramverk för att hasha och stora dom i vår jsonfil,
 router.post('/login', (req, res) => {
     const  { username, password } = req.body;
-
     // gå in i userData jsonfil, och kolla users, username, condition för att kolla om password är korrekt utfärda token, annars felkod 401.
-    const user = userData.users.find((u) => u.username === username);
-
+    const user = userData.users.find((users) => users.username === username);
     //Kommenterar bort kod för att jämföra user hashning av lösenord vi får lägga till denna senare,
     //just nu tittar den i user.json filen om username och lösenord existerar och stämmer överens så generera token och logga in,
-      /* if( user && bcrypt.compareSync(password, user.password)) */
+      
+    /* if( user && bcrypt.compareSync(password, user.password)) */
       if(user) {
         //Om Password och user är korrekt ska vi sätta utfärda ett JWT Token, med en expireDate på 1h, annars skickar vi felkod 401. att loggin blev fel!
-        const token = jwt.sign({ userId: user.id, username: user.username, role: user.role }, 'secretKey', { expiresIn: '1h' });
+        const token = jwt.sign(
+          { 
+            username: user.username,
+            role: user.role },
+            SECRET_KEY || 'yourFallbackSecretKey',
+          { expiresIn: '24h' });
+
         console.log('Token Genererad:', token)
         res.json({ token });
       } else {
@@ -42,13 +45,13 @@ function authCheckToken(req, res, next) {
   const authHeader = req.headers['authorization'];
   const token = authHeader && authHeader.split(' ')[1];
   console.log('Received token for verification:', token);
-
+ 
   //condition om inget token skicka felmeddelande
-  if(!token) {
+     if(!token) {
     return res.status(401).json({ message: 'Unauthorized!' });
   }
  //Verifiera jwt token + secret key skicka annars error om token inte är valid. 
-  jwt.verify(token, secretKey, (err, user) => {
+   jwt.verify(token, SECRET_KEY, (err, user) => {
     if(err) {
       console.error('JWT Verification Error:', err);
       return res.status(403).json({ message: 'Token is not valid!', error: err.message});
@@ -59,28 +62,8 @@ function authCheckToken(req, res, next) {
 }
 
 // Verifiera Token endpoint
-router.get('/verify', authCheckToken, (req, res) => {
-  console.log('Received request for /secret-data:', req.user);
+  router.get('/data', authCheckToken, (req, res) => {
   res.json(req.user);
 });
-
-/* router.post('/login', (req, res) => {
-  const { email, password } = req.body;
-
-  if (isValidUser(email, password)) {
-    const token = jwt.sign({ email, role: getUserRole(email) }, 'secretKey', { expiresIn: '1h' });
-    res.json({ token });
-  } else {
-    res.status(401).json({ error: 'Invalid login' });
-  }
-});
-
-const isValidUser = (email, password) => {
-  return email === 'user@example.com' && password === 'password';
-};
-
-const getUserRole = (email) => {
-  return email === 'admin@example.com' ? 'admin' : 'user';
-}; */
 
 module.exports = router;
